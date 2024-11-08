a9a1dff54c50ed028279b81e1e79bf2fbaf8496f
att124 <daniel.fernandez@davinci.edu.ar>
2024-11-08 15:41:59 -0300

    Revisar transacción

--- procedures/SP_Agregar_Artista.sql
+++ procedures/SP_Agregar_Artista.sql
@@ -5,25 +5,67 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_artista`(
 )
 BEGIN
 
+
 declare v_existe int;
 declare v_resultado varchar(100);
 
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
-        SET v_resultado = 'Hubo un error al intentar agregar el artista.';
 
-set v_existe = (select count(artista.nombre) from artista where artista.nombre = p_nombre);
+
+
+   DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+	BEGIN
+    SET v_resultado = 'Hubo un error al intentar agregar el artista.';
+    ROLLBACK;
+    SELECT v_resultado AS resultado;
+	END;
+    
+    START TRANSACTION;
+
+    Select COUNT(*) into v_existe from artista where trim(nombre) = trim(p_nombre);
+
+
   
-	if v_existe > 0 then
+	if v_existe > 0 then /* Verifica que el artista no se encuentre repetido en la base de datos */
     
 		select "El artista ya se encuentra agregado";
-	
+		rollback;
     else
-    
-		INSERT INTO artista (nombre, nacionalidad, descripcion)
-		VALUES (p_nombre, p_nacionalidad, p_descripcion);
+	    
+			if not p_nombre REGEXP '^[A-Za-z ]+$' THEN /* Verifica que el nombre del artista no se pueda ingresar numeros o carácteres especiales */
         
-        SELECT 'El artista fue agregado exitosamente.' AS resultado;
+			Select 'El nombre no puede contener numeros o carácteres especiales';
+		
+			Rollback;
+
+			else
+        
+					if Length(trim(p_nacionalidad)) > 0 and length(trim(p_nombre)) > 0 then /* Verifica que no se pueda ingresar cadenas vacias en el nombre y en la nacionalidad */
+			
+					Insert into artista (nombre, nacionalidad, descripcion) values (trim(p_nombre), trim(p_nacionalidad), p_descripcion);
+			
+					commit;
+        
+					Select 'El artista fue agregado exitosamente.' AS resultado;
 	
+					else 
+	
+								rollback;
+         
+								if Length(trim(p_nacionalidad)) = 0 then
+					
+								Select 'No se ha ingresado ninguna nacionalidad';
+								
+                                else
+            
+								select 'No se ha ingresado ningun nombre';
+            
+								end if;
+		
+		
+					end if;
+		
+			end if;
+    
     end if;
 
     
