f87bb6898def0bba5c4a62e4619c4cfac87ea5e8
Agustin Aguero <agustin.aguero@davinci.edu.ar>
2024-11-08 19:06:42 -0300

    correccion cliente x usuario

--- procedures/sp_buscar_usuario_por_id.sql
+++ procedures/sp_buscar_usuario_por_id.sql
@@ -1,5 +1,5 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_buscar_cliente_por_id`(
 	IN p_id INT)
 BEGIN
-    SELECT * FROM clientes WHERE id = p_id;
+    SELECT * FROM usuario WHERE id_usuario = p_id;
 END
